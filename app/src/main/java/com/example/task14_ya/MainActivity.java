3b6f2bd35e6367fba1c6513c579813e1dac4480e
בר אופק <u9054555@bsmch.net>
2026-01-22 11:52:54 +0200

    העברה למסך השני

--- app/src/main/java/com/example/task14_ya/MainActivity.java
+++ app/src/main/java/com/example/task14_ya/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.task14_ya;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,46 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1;
+    EditText edit2;
+    Switch switchAnswer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        edit1 = findViewById(R.id.editTextNumberDecimal);
+        edit2 = findViewById(R.id.editTextNumberDecimal2);
+        switchAnswer = findViewById(R.id.switchAnswer);
+    }
 
+    public boolean isValidInput() {
+        String str1 = edit1.getText().toString();
+        String str2 = edit2.getText().toString();
+        if (str1.isEmpty() || str2.isEmpty()) {
+            return false;
+        }
+
+        if (str1.equals(".") || str1.equals("-") || str2.equals(".") || str2.equals("-")) {
+            return false;
+        }
+        return true;
+    }
+
+    public void go(View view)
+    {
+        if (isValidInput()) {
+            double first = Double.parseDouble(edit1.getText().toString());
+            double diff = Double.parseDouble(edit2.getText().toString());
+            boolean isArithmetic = switchAnswer.isChecked();
+
+            Intent intent = new Intent(this, result.class);
+            intent.putExtra("first", first);
+            intent.putExtra("diff", diff);
+            intent.putExtra("isArithmetic", isArithmetic);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/task14_ya/result.java
+++ app/src/main/java/com/example/task14_ya/result.java
@@ -1,6 +1,14 @@
 package com.example.task14_ya;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +16,78 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class result extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class result extends AppCompatActivity
+{
+    ListView lv;
+    TextView tv;
+    ArrayList<Double> sidraArr = new ArrayList<>();
+    ArrayAdapter<Double> adp;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        lv = findViewById(R.id.listItem);
+        tv = findViewById(R.id.textView);
+
+
+        Intent gi = getIntent();
+        double first = gi.getDoubleExtra("first", 0);
+        double diff = gi.getDoubleExtra("diff", 0);
+        boolean isArithmetic = gi.getBooleanExtra("isArithmetic", true);
+        double currentElement = first;
+        sidraArr.clear();
+        for (int i = 0; i < 20; i++)
+        {
+            sidraArr.add(currentElement);
+            if (isArithmetic)
+            {
+                currentElement = currentElement + diff;
+            }
+            else
+            {
+                currentElement = currentElement * diff;
+            }
+        }
+        adp = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sidraArr);
+        lv.setAdapter(adp);
+        registerForContextMenu(lv);
+
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        // כותרת לתפריט הקטן
+        menu.setHeaderTitle("choose");
+        menu.add("mikom");
+        menu.add("shom");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        int position = info.position;
+        String selectedOption = item.getTitle().toString();
+        if (selectedOption.equals("mikom"))
+        {
+            tv.setText("האיבר נמצא במקום ה-" + (position + 1));
+        }
+        else if (selectedOption.equals("shom"))
+        {
+            double sum = 0;
+            for (int i = 0; i <= position; i++)
+            {
+                sum = sum + sidraArr.get(i);
+            }
+            tv.setText("סכום הסדרה עד האיבר הנבחר: " + sum);
+        }
+
+        return true;
     }
 }
